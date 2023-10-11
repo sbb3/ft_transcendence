00aaf221f92b29a2b0133f19e4f35c49594bb4aa
Anas Douib <adouib@e2r12p11.1337.ma>
2023-10-11 21:14:01 +0100

    handled the uploading

--- backend/src/cloudinary/cloudinary.controller.ts
+++ backend/src/cloudinary/cloudinary.controller.ts
@@ -1,26 +1,21 @@
-
-// import { v2 as cloudinary } from 'cloudinary';
-
-// export const CloudinaryProvider = {
-//   provide: 'CLOUDINARY',
-//   useFactory: () => {
-//     return cloudinary.config({
-//         cloud_name: process.env.CLOUDINARY_NAME,
-//         api_key: process.env.CLOUDINARY_API_KEY,
-//         api_secret: process.env.CLOUDINARY_API_SECRET,
-//     });
-//   },
-// };
-
+import { cleanEnv, str } from 'envalid';
 import { v2 } from 'cloudinary';
-// import { CLOUDINARY } from './constants';
+
+const env = cleanEnv(process.env, {
+  CLOUDINARY_NAME: str(),
+  CLOUDINARY_API_KEY: str(),
+  CLOUDINARY_API_SECRET: str(),
+});
+
 export const CloudinaryProvider = {
-  provide: "CLOUDINARY",
-   useFactory: ()=> {
+  provide: 'CLOUDINARY',
+  useFactory: () => {
+    console.log(`\n env.CLOUDINARY_NAME : `, env.CLOUDINARY_NAME, `\n`);
+
     return v2.config({
-      cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
+      cloud_name: env.CLOUDINARY_NAME,
+      api_key: env.CLOUDINARY_API_KEY,
+      api_secret: env.CLOUDINARY_API_SECRET,
     });
   },
 };
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,10 +1,17 @@
-import { Controller, HttpException, HttpStatus, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prismaFolder/prisma.service'; // Import your Prisma service
 import { User } from './entities/user.entity';
-import {Cloudinary} from "@cloudinary/url-gen";
-import {Request} from 'express';
+import { Cloudinary } from '@cloudinary/url-gen';
+import { Request } from 'express';
 import { CloudinaryService } from '../cloudinary/cloudinary.service';
-const cld = new Cloudinary({cloud: {cloudName: 'dtsihtgwx'}});
+const cld = new Cloudinary({ cloud: { cloudName: 'dtsihtgwx' } });
 
 @Injectable()
 export class UsersService {
@@ -15,14 +22,13 @@ export class UsersService {
   private readonly logger = new Logger(Controller.name);
 
   constructor(
-    private readonly prismaService: PrismaService,
-    private PrismaService: PrismaService,
+    private prismaService: PrismaService,
     private cloudinaryService: CloudinaryService,
   ) {}
   isUserExist = (user: User | null): user is User => {
-		return user !== null;
-	}
-  
+    return user !== null;
+  };
+
   async findAllUsers() {
     const users = await this.prismaService.user.findMany();
     return users;
@@ -41,20 +47,18 @@ export class UsersService {
   }
 
   async findByUsername(usernames: string) {
-  
-      const user = await this.prismaService.user.findUnique({
-        where: {
-          username: usernames,
-        },
-      });
-      
-      if (!user) {
-        throw new NotFoundException(`User with ID ${usernames} not found`);
-      }
-      return user;
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        username: usernames,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${usernames} not found`);
+    }
+    return user;
   }
-    
-    
+
   async findByEmail(emails: string) {
     const user = await this.prismaService.user.findUnique({
       where: {
@@ -62,46 +66,38 @@ export class UsersService {
       },
     });
 
-    if (this.isUserExist(user))
-			return user;
-		else
-			throw new NotFoundException('user not found');
+    if (this.isUserExist(user)) return user;
+    else throw new NotFoundException('user not found');
   }
 
-
-
-
-  async updateUserDetails(userId: number, username: string, file: Express.Multer.File): Promise<void> {
-    this.logger.log('\nupdateUserDetails\n');
-    console.log(`\n userId : ${userId} \n`);
-    console.log(`\n username : ${username} \n`);
-    // console.log(`\n filename : ${file.filename} \n`);
-    console.log(`\n file : ${JSON.stringify(file.originalname)} \n`);
-    console.log(`\n path  : ${JSON.stringify(file.path)} \n`);
-
-    // const avatarUrl = await this.cloudinaryService.uploadImage(file);
-    const rest = await this.cloudinaryService.uploadImage(file).catch(() => {
-      throw new Error('Invalid file type.');
-    });
-
-    console.log(`\n rest : ${JSON.stringify(rest)} \n`);
-    // this.logger.log(`\n avatarUrl : ${avatarUrl} \n`);
-    this.logger.log(`\n username : ${username} \n`);
-    await this.PrismaService.user.update({
-      where: { id: userId },
+  async updateUserDetails(
+    userId: number,
+    username: string,
+    file: Express.Multer.File,
+  ): Promise<void> {
+    const rest = await this.cloudinaryService.uploadImage(file);
+    const avatarUrl = rest.secure_url;
+    await this.prismaService.user.update({
+      where: { id: parseInt(userId.toString(), 10) },
       data: {
-        username : username,
-        // avatar: avatarUrl,
+        username: username,
+        avatar: avatarUrl,
       },
     });
+  }
+
+  async getCurrentUser(userId: number) {
+    console.log(`userId: ${userId} `);
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: { id: parseInt(userId.toString(), 10) },
+      });
+      if (!user) {
+        throw new NotFoundException(`User with id ${userId} not found`);
+      }
+      return user;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
-
-// async uploadImageToCloudinary(file: Express.Multer.File) {
-//   return await this.cloudinaryService.uploadImage(file).catch(() => {
-//     throw new Error('Invalid file type.');
-//   });
-// }
-
-
-}
-
